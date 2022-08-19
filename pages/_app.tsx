d6726af299fe75f6e7681a0b9717e16134a32019
Nerios Lamaj <nerios.lamaj@gmail.com>
2022-08-19 12:34:50 +0200

    Adding Geist UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
+
 import '../styles/globals.css'
 import 'nextra-theme-docs/style.css'
 
 function Nextra({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
 }
 
 export default Nextra
